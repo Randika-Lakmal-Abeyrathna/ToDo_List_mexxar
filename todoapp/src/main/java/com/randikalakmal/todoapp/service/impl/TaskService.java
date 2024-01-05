719a80ab11713c839e0acacbebfc5da0b9090ab9
Randika Lakmal <randika.l.abeyrathna@gmail.com>
2024-01-05 15:57:10 +0530

    Base Service Fixed

--- todoapp/src/main/java/com/randikalakmal/todoapp/service/impl/TaskService.java
+++ todoapp/src/main/java/com/randikalakmal/todoapp/service/impl/TaskService.java
@@ -2,6 +2,7 @@ package com.randikalakmal.todoapp.service.impl;
 
 import com.randikalakmal.todoapp.Exceptions.TaskException;
 import com.randikalakmal.todoapp.model.Task;
+import com.randikalakmal.todoapp.repository.BaseRepository;
 import com.randikalakmal.todoapp.repository.TaskRepository;
 import com.randikalakmal.todoapp.service.BaseService;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class TaskService implements BaseService {
+public class TaskService extends BaseService<Task,Long> {
 
     private final TaskRepository taskRepository;
 
@@ -17,29 +18,8 @@ public class TaskService implements BaseService {
         this.taskRepository = taskRepository;
     }
 
-
     @Override
-    public Task create(Object task) {
-        return taskRepository.save((Task) task);
-    }
-
-    @Override
-    public Task update(Object task) {
-        return taskRepository.save((Task) task);
-    }
-
-    @Override
-    public void delete(Object task) {
-        taskRepository.delete((Task) task);
-    }
-
-    @Override
-    public List<Task> getAll() {
-        return taskRepository.findAll();
-    }
-
-    @Override
-    public Task getById(Object id) {
-        return taskRepository.findById((Long) id).orElseThrow(()-> new TaskException("Task id not found "+id));
+    protected BaseRepository<Task, Long> getRepository() {
+        return taskRepository;
     }
 }
--- todoapp/src/main/java/com/randikalakmal/todoapp/service/impl/UserService.java
+++ todoapp/src/main/java/com/randikalakmal/todoapp/service/impl/UserService.java
@@ -3,6 +3,7 @@ package com.randikalakmal.todoapp.service.impl;
 import com.randikalakmal.todoapp.DTO.UserDTO;
 import com.randikalakmal.todoapp.Exceptions.UserExceptions;
 import com.randikalakmal.todoapp.model.User;
+import com.randikalakmal.todoapp.repository.BaseRepository;
 import com.randikalakmal.todoapp.repository.UserRepository;
 import com.randikalakmal.todoapp.service.BaseService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Service
-public class UserService implements BaseService {
+public class UserService extends BaseService<User,Long> {
 
     private final UserRepository userRepository;
 
@@ -21,33 +22,8 @@ public class UserService implements BaseService {
     }
 
     @Override
-    public User create(Object user) {
-
-        User user1 = (User) user;
-        user1.setCreatedDate(LocalDate.now());
-
-        return userRepository.save((User) user);
+    protected BaseRepository getRepository() {
+        return userRepository;
     }
 
-    @Override
-    public User update(Object user) {
-
-        return userRepository.save((User) user);
-    }
-
-    @Override
-    public void delete(Object user) {
-
-         userRepository.delete((User) user);
-    }
-
-    @Override
-    public List<User> getAll() {
-        return userRepository.findAll();
-    }
-
-    @Override
-    public User getById(Object id) {
-        return userRepository.findById((Long) id ).orElseThrow(() ->new UserExceptions("User Not found with id "+id));
-    }
 }
